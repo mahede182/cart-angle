2c1e7a43f178bf1476b1261354fd47010cf7bfaf
mahede <mahede.object@gmail.com>
2021-11-15 15:39:51 -1200

    solve router basename

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     )
   }
   return (
-    <Router>
+    <Router basename={process.env.NODE_ENV==='production'?'/reactjs-deploy':''}>
       <main>
         <Navbar />
         <CartContainer />
